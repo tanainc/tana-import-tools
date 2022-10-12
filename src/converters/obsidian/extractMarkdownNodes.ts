cc8baa95595234ee65a7a4013d079720b799bf3f
Lucca Hellriegel <lucca.hellriegel@gmx.net>
2022-10-12 20:39:15 +0200

    Obsidian: treat tab properly in outline nodes (2)

--- src/converters/obsidian/extractMarkdownNodes.ts
+++ src/converters/obsidian/extractMarkdownNodes.ts
@@ -170,7 +170,7 @@ function findEndPosition(content: string, curPosition: number, hierarchy: Hierar
 
 export function countEmptySpace(content: string, curPosition: number, count = 0): number {
   //we count tab as one empty space
-  if (isEmptySpace(content[curPosition])) {
+  if (!isEmptySpace(content[curPosition])) {
     return count;
   }
   return countEmptySpace(content, curPosition + 1, count + 1);

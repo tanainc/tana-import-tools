4b4b55103dc0cb24ea4b2df7c1eefc7412af5955
Ian Jones <ijones@splashfinancial.com>
2022-10-14 12:35:09 -0600

    fields test

--- src/converters/notion/tests/notion.test.ts
+++ src/converters/notion/tests/notion.test.ts
@@ -3,115 +3,105 @@
 import { expect, test } from '@jest/globals';
 import { expectField, expectImage } from '../../../testUtils/testUtils';
 import { importNotionFile } from './testUtils';
-test.only('Smoke test import preview ', async () => {
-    const [file] = importNotionFile('singleNode.csv');
-  
-    expect(file.summary).toEqual({
-        "brokenRefs": 0,
-        "calendarNodes": 0,
-        "fields": 8,
-        "leafNodes": 9,
-        "topLevelNodes": 1,
-        "totalNodes": 10,
-    });
+test('Smoke test import preview ', async () => {
+  const [file] = importNotionFile('singleNode.csv');
 
-
-  
-    expect(file.attributes).toEqual([
-         {
-         "count": 1,
-         "name": "Number",
-         "values":  [
-           "1",
-         ],
-       },
-        {
-         "count": 1,
-         "name": "Entry",
-         "values":  [
-           "#001",
-         ],
-       },
-        {
-         "count": 1,
-         "name": "Gen",
-         "values":  [
-           "1",
-         ],
-       },
-        {
-         "count": 1,
-         "name": "Generation",
-         "values":  [
-           "Generation 1",
-         ],
-       },
-        {
-         "count": 1,
-         "name": "Name",
-         "values":  [
-           "Bulbasaur",
-         ],
-       },
-        {
-         "count": 1,
-         "name": "Japanese Name",
-         "values":  [
-           "Fushigidaneフシギダネ",
-         ],
-       },
-        {
-         "count": 1,
-         "name": "Classfication",
-         "values":  [
-           "Seed Pokémon",
-         ],
-       },
-        {
-         "count": 1,
-         "name": "Favourite",
-         "values":  [
-           "No",
-         ],
-       },
-    ]);
+  expect(file.summary).toEqual({
+    brokenRefs: 0,
+    calendarNodes: 0,
+    fields: 8,
+    leafNodes: 9,
+    topLevelNodes: 1,
+    totalNodes: 10,
   });
 
-test.only('Reports Broken link in preview', async () => {
-    const [file] = importNotionFile('brokenRef.csv');
-  
-    expect(file.summary).toEqual({
-        "brokenRefs": 1,
-        "calendarNodes": 0,
-        "fields": 3,
-        "leafNodes": 4,
-        "topLevelNodes": 1,
-        "totalNodes": 5,
-    });
+  expect(file.attributes).toEqual([
+    {
+      count: 1,
+      name: 'Number',
+      values: ['1'],
+    },
+    {
+      count: 1,
+      name: 'Entry',
+      values: ['#001'],
+    },
+    {
+      count: 1,
+      name: 'Gen',
+      values: ['1'],
+    },
+    {
+      count: 1,
+      name: 'Generation',
+      values: ['Generation 1'],
+    },
+    {
+      count: 1,
+      name: 'Name',
+      values: ['Bulbasaur'],
+    },
+    {
+      count: 1,
+      name: 'Japanese Name',
+      values: ['Fushigidaneフシギダネ'],
+    },
+    {
+      count: 1,
+      name: 'Classfication',
+      values: ['Seed Pokémon'],
+    },
+    {
+      count: 1,
+      name: 'Favourite',
+      values: ['No'],
+    },
+  ]);
 });
 
-test.only('Handles notion exporting zero width characters', async () => {
-    const [file] = importNotionFile('zeroWidthNoBreakFile.csv');
-  
-    expect(file.summary).toEqual({
-        "brokenRefs": 0,
-        "calendarNodes": 0,
-        "fields": 2,
-        "leafNodes": 3,
-        "topLevelNodes": 1,
-        "totalNodes": 4,
-    });
+test('Reports Broken link in preview', async () => {
+  const [file] = importNotionFile('brokenRef.csv');
 
-    expect(file.attributes).toEqual([
-       {
-        count: 1,
-        name: "Number",
-        values: ["1"]
-       },
-       {
-        count: 1,
-        name: "Name",
-        values: ["Bulbasaur"]
-       }
-   ]);
+  expect(file.summary).toEqual({
+    brokenRefs: 1,
+    calendarNodes: 0,
+    fields: 3,
+    leafNodes: 4,
+    topLevelNodes: 1,
+    totalNodes: 5,
+  });
+});
+
+test('Handles notion exporting zero width characters', async () => {
+  const [file] = importNotionFile('zeroWidthNoBreakFile.csv');
+
+  expect(file.summary).toEqual({
+    brokenRefs: 0,
+    calendarNodes: 0,
+    fields: 2,
+    leafNodes: 3,
+    topLevelNodes: 1,
+    totalNodes: 4,
+  });
+
+  expect(file.attributes).toEqual([
+    {
+      count: 1,
+      name: 'Number',
+      values: ['1'],
+    },
+    {
+      count: 1,
+      name: 'Name',
+      values: ['Bulbasaur'],
+    },
+  ]);
+});
+
+test('fields', () => {
+  const [_file, _f, fn] = importNotionFile('singleNode.csv');
+  expectField('Number', 'Number', ['1'], fn);
+  expectField('Entry', 'Entry', ['#001'], fn);
+  expectField('Gen', 'Gen', ['1'], fn);
+  expectField('Name', 'Name', ['Bulbasaur'], fn);
 });
